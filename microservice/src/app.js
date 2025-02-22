ff9d5168675b21d402ddc598c244e751f4806b08
nevillew <11432018+nevillew@users.noreply.github.com>
2025-02-23 09:54:07 +1000

    refactor: Remove duplicate code in app.js

--- microservice/src/app.js
+++ microservice/src/app.js
@@ -29,34 +29,3 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-const { errorHandler } = require('./middleware/errorHandler');
-const routes = require('./routes');
-const logger = require('./utils/logger');
-const { validateToken } = require('./middleware/auth');
-
-const app = express();
-
-// Security middleware
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-
-// Auth middleware for all routes
-app.use(validateToken);
-
-// Routes
-app.use('/api', routes);
-
-// Error handling
-app.use(errorHandler);
-
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
-});
-
-module.exports = app;
