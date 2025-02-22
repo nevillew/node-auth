c7542e13c9bc15fedaad2e58bce025e042afa998
nevillew <11432018+nevillew@users.noreply.github.com>
2025-02-23 09:49:19 +1000

    fix: Remove duplicate code and resolve merge conflicts in database and auth middleware

--- microservice/src/config/database.js
+++ microservice/src/config/database.js
@@ -1,22 +1,3 @@
-const { Sequelize } = require('sequelize');
-const logger = require('../utils/logger');
-
-const sequelize = new Sequelize({
-  database: process.env.DB_NAME,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'postgres',
-  logging: msg => logger.debug(msg),
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-});
-
 module.exports = sequelize;
 const { Sequelize } = require('sequelize');
 const logger = require('../utils/logger');
